ab98ffc5411c5c8373b9073cd898486afcbdb058
JehunYoo <percyu98@gmail.com>
2023-09-02 15:41:42 +0900

    [java] Solve 9019

--- 9019.java
+++ 9019.java
@@ -16,6 +16,88 @@ public class Main {
 
             Queue<Register> queue = new ArrayDeque<>();
             queue.add(new Register(A, ""));
+            boolean[] visited = new boolean[10000];
+            visited[A] = true;
+
+            while (queue.size() > 0) {
+                Register register = queue.remove();
+                int value = register.value;
+                String s = register.s;
+
+                if (value == B) {
+                    bw.write(s + "\n");
+                    break;
+                }
+
+                int D = Register.operateD(value);
+                if(!visited[D]) {
+                    queue.add(new Register(D, s + "D"));
+                    visited[D] = true;
+                }
+                int S = Register.operateS(value);
+                if(!visited[S]) {
+                    queue.add(new Register(S, s + "S"));
+                    visited[S] = true;
+                }
+                int L = Register.operateL(value);
+                if(!visited[L]) {
+                    queue.add(new Register(L, s + "L"));
+                    visited[L] = true;
+                }
+                int R = Register.operateR(value);
+                if(!visited[R]) {
+                    queue.add(new Register(R, s + "R"));
+                    visited[R] = true;
+                }
+            }
+        }
+
+        bw.flush();
+        bw.close();
+    }
+    static class Register {
+        int value;
+        String s;
+
+        public Register(int value, String s) {
+            this.value = value;
+            this.s = s;
+        }
+
+        public static int operateD(int value) {
+            return (value * 2) % 10000;
+        }
+
+        public static int operateS(int value) {
+            return value == 0? 9999: value - 1;
+        }
+
+        public static int operateL(int value) {
+            return (value % 1000) * 10 + value / 1000;
+        }
+
+        public static int operateR(int value) {
+            return (value % 10) * 1000 + value / 10;
+        }
+    }
+}
+
+// HashSet takes more time than array
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int t=0; t<T; t++) {
+            String[] in = br.readLine().split(" ");
+            int A = Integer.parseInt(in[0]);
+            int B = Integer.parseInt(in[1]);
+
+            Queue<Register> queue = new ArrayDeque<>();
+            queue.add(new Register(A, ""));
             Set<Integer> visited = new HashSet<>();
             visited.add(A);
 
